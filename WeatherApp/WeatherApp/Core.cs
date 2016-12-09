256a4852d5b9651a85996dea1c374edcf95e502d
a15995 <123@123.dk>
2016-12-09 14:08:22 +0100

    Indtast Bynavn

---
+++ WeatherApp/WeatherApp/Core.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class Core
+    {
+        public static async Task<Weather> GetWeather(string zipCode)
+        {
+            //Sign up for a free API key at http://openweathermap.org/appid
+            string key = "46fdd4e00701a03879f3979af8d99540";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?q="
+                + zipCode + "&units=metric&appid=" + key;
+
+            var results = await DataService.getDataFromService(queryString).ConfigureAwait(false);
+
+            if (results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + " C";
+                weather.Wind = (string)results["wind"]["speed"] + " km/t";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
