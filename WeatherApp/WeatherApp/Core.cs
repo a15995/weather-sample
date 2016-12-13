5a9027ac54d761ac249cf823de35d58df756087c
Søren Larsen <123@123.dk>
2016-12-13 19:33:29 +0100

    API KEY

--- WeatherApp/WeatherApp/Core.cs
+++ WeatherApp/WeatherApp/Core.cs
@@ -8,7 +8,7 @@ namespace WeatherApp
         public static async Task<Weather> GetWeather(string zipCode)
         {
             //Sign up for a free API key at http://openweathermap.org/appid
-            string key = "API key";
+            string key = "YOUR API KEY HERE";
             string queryString = "http://api.openweathermap.org/data/2.5/weather?q="
                 + zipCode + "&units=metric&appid=" + key;
 
